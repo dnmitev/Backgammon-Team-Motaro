5af94064c0f666dfdb44dcfd94650bec0908c4db
desislavaplam <desislava.plam@gmail.com>
2014-06-09 23:11:18 +0300

    Adding Board.js
    Adding the board, This will be the coordinates. first triangle starts at
    x = 63 and y = 40 for the top and y = 560 for the bottom. The base of
    the triangle is 55, the height is 220.

---
+++ Scripts/Board.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="_references.js" />
+
+var field = Raphael(0, 0, 840, 600);
+
+var frame = field.rect(0, 0, 840, 600)
+            .attr({
+                fill: 'lightblue'
+            });
+var playField = field.rect(63, 40, 715, 520)
+            .attr({
+                fill: 'white'
+            });
+for (var x = 63; x < 778; x += 55) {
+    if (x !== 393) {
+
+        field.path('M ' + x + ' 40 L ' + (x + 27) + ' 260 L ' + (x + 55) + ' 40');
+
+        field.path('M ' + x + ' 560 L ' + (x + 27) + ' 340 L ' + (x + 55) + ' 560');
+
+    }
+}
